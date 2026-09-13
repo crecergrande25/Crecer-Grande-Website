4a10b8b0ae96e8f27c3552becdd34c8173868912
crecergrande25 <crecergrande@outlook.com>
2026-09-13 19:08:32 +0530

    Pushing v1.9.3

--- assets/js/backend-config.js
+++
@@ -1,6 +0,0 @@
-window.CG_BACKEND = {
-  "enabled": true,
-  "url": "https://nznjqglgiezjehutdsbp.supabase.co",
-  "publishableKey": "sb_publishable_4B16fVwvlWYc2J2DDBVbaw_k99IJTgv",
-  "siteVersion": "1.9.3"
-};
